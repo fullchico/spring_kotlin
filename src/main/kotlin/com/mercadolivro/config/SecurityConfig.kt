d5c797fa6b7e528ec722cafb0847d0978dd957e9
FullBarbosa <diibarbosa22@gmail.com>
2021-10-06 20:44:03 -0300

    authorization

--- src/main/kotlin/com/mercadolivro/config/SecurityConfig.kt
+++ src/main/kotlin/com/mercadolivro/config/SecurityConfig.kt
@@ -2,6 +2,7 @@ package com.mercadolivro.config
 
 import com.mercadolivro.repository.CustomerRepository
 import com.mercadolivro.security.AuthenticationFilter
+import com.mercadolivro.security.AuthorizationFilter
 import com.mercadolivro.security.JwtUtil
 import com.mercadolivro.service.UserDetailsCustomeService
 import org.springframework.context.annotation.Bean
@@ -21,15 +22,15 @@ class SecurityConfig(
     private val userDetails: UserDetailsCustomeService,
     private val jwtUtil: JwtUtil
 ) : WebSecurityConfigurerAdapter() {
-
+    
     private val PUBLIC_POST_MATCHERS = arrayOf(
         "/customers"
     )
-
+    
     override fun configure(auth: AuthenticationManagerBuilder) {
         auth.userDetailsService(userDetails).passwordEncoder(bCryptPasswordEncoder())
     }
-
+    
     override fun configure(http: HttpSecurity) {
         http.cors().and().csrf().disable()
         http.authorizeRequests()
@@ -37,9 +38,10 @@ class SecurityConfig(
             .antMatchers(HttpMethod.POST, *PUBLIC_POST_MATCHERS).permitAll()
             .anyRequest().authenticated()
         http.addFilter(AuthenticationFilter(authenticationManager(), customerRepository, jwtUtil))
+        http.addFilter(AuthorizationFilter(authenticationManager(), userDetails, jwtUtil))
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
     }
-
+    
     @Bean
     fun bCryptPasswordEncoder(): BCryptPasswordEncoder {
         return BCryptPasswordEncoder()
